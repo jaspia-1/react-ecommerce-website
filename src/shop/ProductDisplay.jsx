7f93283c0d330ec4681801f6cb921ef76cf78379
j-aspia-1 <taspiak049@gmail.com>
2024-01-07 14:24:24 +0600

    second commit

---
+++ src/shop/ProductDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductDisplay = ({item}) => {
+    console.log(item)
+    return (
+        <div>
+            ProductDisplay
+        </div>
+    );
+};
+
+export default ProductDisplay;
