4b5afed101adec34f88e6b000ba1a5b37f311e21
jaspia-1 <taspiak049@gmail.com>
2024-01-13 22:42:37 +0600

    authProvider

--- src/main.jsx
+++ src/main.jsx
@@ -25,6 +25,8 @@ import CartPage from './shop/CartPage.jsx';
 import SingleBlog from './Blog/SingleBlog.jsx';
 import About from './about/About.jsx';
 import ContactPage from './contactPage/ContactPage.jsx';
+import AuthProvider from './contexts/AuthProvider.jsx';
+
 
 const router = createBrowserRouter([
   {
@@ -66,8 +68,11 @@ const router = createBrowserRouter([
       }
     ]
   },
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 )
